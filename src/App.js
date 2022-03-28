ea98b522aa700598fea674d4842e8d605499c803
Colten Empey <colten@empey.me>
2022-03-27 21:28:51 -0600

    all finished up. did it a little later but got it down.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Character from "./components/Character"
 
 const App = () => {
+  
+  const [details, setDetails] = useState("")
+  const [active, setActive] = useState("")
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  // const postData = (arr) =>{
+  //   arr.map( char =>{
+  //     setCharacter(char.name)
+      
+  //   })
+  // }
+  
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      <Character/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,69 @@
 // Write your Character component here
+import axios from "axios"
+import React, {useState, useEffect} from 'react';
+
+const Character = () =>{
+    const [characters, setCharacters] = useState([])
+    useEffect(() =>{
+        axios.get("https://swapi.dev/api/people/")
+        .then( res=>{
+            const returnArray = res.data
+            console.log(returnArray)
+            setCharacters(returnArray)
+            // returnArray.map( char =>{
+
+            //     setCharacter(char.name)
+            //     return character
+            // })
+        })
+        .catch(err => {
+            console.error(err)
+        })
+    },[])
+
+    const listItems = characters.map((character, index) => (
+      <div class="accordion-item" key={`accordion${index}`}>
+        <h2 class="accordion-header" id={`heading${index}`}>
+          <button
+            class="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target={`#collapse${index}`}
+            aria-expanded= {index === 0 ? "true" : "false"}
+            aria-controls={`collapse${index}`}
+          >
+            {character.name}
+          </button>
+        </h2>
+        <div
+          id={`collapse${index}`}
+          class="accordion-collapse collapse show"
+          aria-labelledby={`heading${index}`}
+          data-bs-parent="#accordionExample"
+        >
+          <div class="accordion-body">
+            <p>
+                Gender: {character.gender} <br />
+                Height: {character.height} <br />
+                Hair Color: {character.hair_color} <br />
+                Eye Color: {character.eye_color} <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    ));
+    
+            
+            // <div>{character.name}</div>
+        
+    
+    console.log(characters)
+    return (
+        <div class="accordion" id="charcters">
+        {listItems}
+        </div>
+    )
+} 
+
+
+export default Character
